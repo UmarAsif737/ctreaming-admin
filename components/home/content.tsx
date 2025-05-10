8c977ce8ba784ddc62c2cb17f2c50e6430bf418e
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-05-11 01:28:30 +0500

    `Removed unused code in content of dashboard page (commented code)`

--- components/home/content.tsx
+++ components/home/content.tsx
@@ -69,52 +69,6 @@ export const Content = ({ stats }: { stats: any }) => (
         />
       </div>
     </div>
-    <div className="flex justify-center gap-4 xl:gap-6 pt-3 px-4 lg:px-0 flex-wrap xl:flex-nowrap sm:pt-10 max-w-[90rem] mx-auto w-full">
-      {/* <div className="mt-6 gap-6 flex flex-col w-full">
-        <div className="h-full flex flex-col gap-2">
-          <h3 className="text-xl font-semibold">Total Counts</h3>
-          <div className="w-full bg-default-50 shadow-lg rounded-2xl p-6">
-            <TotalCountsChart
-              data={[
-                stats.total_users,
-                stats.total_vehicles,
-                stats.total_vehicle_documents,
-                stats.total_driver_documents,
-                stats.total_drivers,
-                stats.total_forms,
-              ]}
-            />
-          </div>
-        </div>
-      </div> */}
-
-      {/* Left Section */}
-      <div className="mt-4 gap-2 flex flex-col xl:max-w-md w-full">
-        {/* <h3 className="text-xl font-semibold">Renewal Requests by Status</h3> */}
-        {/* <div className="w-full bg-default-50 shadow-lg rounded-2xl p-6">
-          <RenewalRequestsChart
-            data={Number(stats.renewal_requests_by_status.request_initiated)}
-          />
-        </div> */}
-
-        {/* Pie Chart */}
-        {/* <div className="h-full flex flex-col gap-2">
-          <h3 className="text-xl font-semibold">Statistics</h3>
-          <div className="w-full bg-default-50 shadow-lg rounded-2xl p-6">
-            <PieChart />
-          </div>
-        </div> */}
-      </div>
-    </div>
-
-    {/* Mixed Statistics Chart */}
-    {/* <div className="flex flex-col justify-center w-full py-5 px-4 lg:px-0 max-w-[90rem] mx-auto gap-3">
-      <div className="h-full flex flex-col gap-2">
-        <h3 className="text-xl font-semibold">Mixed Statistics</h3>
-        <div className="w-full bg-default-50 shadow-lg rounded-2xl p-6">
-          <MixedChart />
-        </div>
-      </div>
-    </div> */}
+    
   </div>
 );
