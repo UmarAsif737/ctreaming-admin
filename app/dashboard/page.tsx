b560a1d7671537abd4b89ff0d1a3f1bcb3e302ac
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-05-10 18:02:47 +0500

    `Dashboard page adjusted a bit`

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,25 +1,25 @@
 // pages/index.tsx
-import { getDashboardStats } from "@/actions/dashboard-stats";
+// import { getDashboardStats } from "@/actions/dashboard-stats";
 import { Content } from "@/components/home/content";
 import dynamic from "next/dynamic";
 
-const TotalCountsChart = dynamic(
-  () => import("@/components/charts/total-counts-charts"),
-  { ssr: false }
-);
-const RenewalRequestsChart = dynamic(
-  () => import("@/components/charts/renewal-request"),
-  { ssr: false }
-);
+// const TotalCountsChart = dynamic(
+//   () => import("@/components/charts/total-counts-charts"),
+//   { ssr: false }
+// );
+// const RenewalRequestsChart = dynamic(
+//   () => import("@/components/charts/renewal-request"),
+//   { ssr: false }
+// );
 
 const Home = async () => {
-  const { data, error } = await getDashboardStats();
+  // const { data, error } = await getDashboardStats();
 
-  if (!data || error) {
-    return <div>Error loading data</div>;
-  }
+  // if (!data || error) {
+  //   return <div>Error loading data</div>;
+  // }
 
-  return <Content stats={data ?? null} />;
+  return <Content stats={null} />;
 };
 
 export default Home;
