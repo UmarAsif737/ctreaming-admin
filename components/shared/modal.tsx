e0ebffb5db2acb4609767e1ba717f0497fc9fb5d
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-05-13 20:14:13 +0500

    `custom modal shared compionent has been added`

---
+++ components/shared/modal.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { ReactNode } from "react";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+  className?: string;
+}
+
+export const Modal = ({ isOpen, onClose, children, className }: ModalProps) => {
+  if (!isOpen) return null; // Prevent rendering when closed
+
+  return (
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-[#0C111D]/50 backdrop-blur-sm px-4 no-scrollbar"
+      onClick={onClose}
+    >
+      <div
+        className={`bg-white rounded-xl w-full max-w-4xl max-h-[90vh] overflow-hidden flex flex-col no-scrollbar ${
+          className ?? ""
+        }`}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {/* Modal scrollable content */}
+        <div className="overflow-y-auto p-6 h-full">{children}</div>
+      </div>
+    </div>
+  );
+};
