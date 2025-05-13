34825f2a3ff00ccade0b9bd06ca9c52203fa2814
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-05-13 20:20:48 +0500

    `FormatBudgetValue helper fn has been added`

--- helpers/utils.ts
+++ helpers/utils.ts
@@ -10,3 +10,14 @@ export function decodeHtmlEntities(text: string) {
 	textarea.innerHTML = text;
 	return textarea.value;
 }
+
+export const FormatBudgetValue = (value: string) => {
+	// Remove all non-digit characters except the decimal point
+	const numeric = value.replace(/[^\d.]/g, "");
+
+	// Format the number with commas
+	const [integer, decimal] = numeric.split(".");
+	const formatted = new Intl.NumberFormat("en-US").format(Number(integer || 0));
+
+	return decimal ? `${formatted}.${decimal}` : `${formatted}`;
+};
